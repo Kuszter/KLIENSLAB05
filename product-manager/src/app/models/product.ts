bc879d0ba61a2e327ae4a2c8f05e46e937d6542a
Kuszter <kusztosdavid@stud.uni-obuda.hu>
2023-05-02 11:51:19 +0200

    Projects and product.ts

---
+++ product-manager/src/app/models/product.ts
@@ -0,0 +1,18 @@
+export class Product {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    discountPercentage: number;
+    rating: number;
+    stock: number;
+    brand: string;
+    category: string;
+    thumbnail: string;
+    images: string[];
+  
+    constructor(data: any) {
+      Object.assign(this, data);
+    }
+  }
+  
